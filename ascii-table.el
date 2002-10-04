422dd23aef5687b2d019651677e39694e4d61e07
Lawrence Mitchell <wence@gmx.li>
2002-10-04 09:40:41 +0000

    Cleaned up, moved to 3-column display.

--- ascii-table.el
+++ ascii-table.el
@@ -1,5 +1,5 @@
 ;;; ascii-table.el --- Print an ASCII Table.
-;; $Id: ascii-table.el,v 1.4 2002/06/17 17:54:21 lawrence Exp $
+;; $Id: ascii-table.el,v 1.5 2002/10/04 09:40:41 lawrence Exp $
 
 ;;; Commentary:
 ;; Vital for any coder :)
@@ -10,6 +10,9 @@
 
 ;;; History:
 ;; $Log: ascii-table.el,v $
+;; Revision 1.5  2002/10/04 09:40:41  lawrence
+;; Cleaned up, moved to 3-column display.
+;;
 ;; Revision 1.4  2002/06/17 17:54:21  lawrence
 ;; Added Commentary.
 ;;
@@ -22,45 +25,51 @@
 ;;
 
 ;;; Code:
-
+(require 'cl)
 (defconst ascii-version
-  "$Id: ascii-table.el,v 1.4 2002/06/17 17:54:21 lawrence Exp $"
+  "$Id: ascii-table.el,v 1.5 2002/10/04 09:40:41 lawrence Exp $"
   "ascii-table's version number.")
 
 (defun ascii-pad-to-column (column string)
   "Pad to COLUMN with spaces, then insert STRING."
-  (while (< (current-column) column)
-    (insert " "))
-  (insert string))
+  (loop while (< (current-column) column)
+        do (insert " ")
+        finally (insert string)))
 
 (defun ascii-table ()
   "Display an ASCII Table."
   (interactive)
   (switch-to-buffer (get-buffer-create "*ASCII Table*"))
   (erase-buffer)
-  (insert (concat
-           " Cha | Dec | Oct  | Hex  || Cha | Dec | Oct  | Hex\n"
-           "-----+-----+------+------++-----+-----+------+------\n"))
-  (let ((char 0))
-    (while (<= char 63)
-      (insert (format " %2s" (single-key-description char)))
-      (ascii-pad-to-column 5 "|")
-      (insert (format " %2d" char))
-      (ascii-pad-to-column 11 "|")
-      (insert (format " %04o" char))
-      (ascii-pad-to-column 18 "|")
-      (insert (format " 0x%02x" char))
-      (ascii-pad-to-column 25 "||")
-      (let ((char1 (+ 64 char)))
-        (insert (format " %2s" (single-key-description char1)))
-        (ascii-pad-to-column 32 "|")
-        (insert (format " %2d" char1))
-        (ascii-pad-to-column 38 "|")
-        (insert (format " %04o" char1))
-        (ascii-pad-to-column 45 "|")
-        (insert (format " 0x%02x\n" char1)))
-      (setq char (1+ char)))))
+  (insert " Cha | Dec | Oct  | Hex  || Cha | Dec | Oct  | Hex  || Cha | Dec | Oct  | Hex\n"
+          "-----+-----+------+------++-----+-----+------+------++-----+-----+------+------\n")
+  (loop for i from 0 to 42
+        for j = (+ i 43)
+        for k = (+ j 43)
+        do
+        (ascii-insert-char i)
+        (ascii-insert-char j)
+        (ascii-insert-char k)))
 
+(defun ascii-insert-char (char)
+  (if (= char 128)
+      (insert "\n")
+    (let ((col (cond ((< char 43)
+                      5)
+                     ((< char 86)
+                      32)
+                     (t
+                      59))))
+      (insert (format " %2s" (single-key-description char)))
+      (ascii-pad-to-column col "|")
+      (insert (format " %2d" char))
+      (ascii-pad-to-column (setq col (+ col 6)) "|")
+      (insert (format " %04o" char))
+      (ascii-pad-to-column (setq col (+ col 7)) "|")
+      (insert (format " 0x%02x" char))
+      (if (> char 85)
+          (insert "\n")
+        (ascii-pad-to-column (setq col (+ col 7)) "||")))))
 
 
 (provide 'ascii-table)
