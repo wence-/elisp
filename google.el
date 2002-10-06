cc699a9a8430118f697cc07b15054ecaf5cd029a
Lawrence Mitchell <wence@gmx.li>
2002-10-06 21:06:06 +0000

    Integrated summer changes, see ChangeLog for details.

--- google.el
+++ google.el
@@ -1,10 +1,11 @@
 ;;; google.el --- Googling for stuff
-;; $Id: google.el,v 1.3 2002/06/17 17:57:39 lawrence Exp $
+;;; $Id: google.el,v 1.4 2002/10/06 21:06:06 lawrence Exp $
+
+;; This file is NOT part of Emacs.
 
 ;; Copyright (C) 2002 lawrence mitchell <wence@gmx.li>
-
 ;; Filename: google.el
-;; Version: $Revision: 1.3 $
+;; Version: $Revision: 1.4 $
 ;; Author: lawrence mitchell <wence@gmx.li>
 ;; Maintainer: lawrence mitchell <wence@gmx.li>
 ;; Created: 2002-05-15
@@ -16,13 +17,12 @@
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2 of the License, or (at
 ;; your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-;; General Public License for more
-;; details. http://www.gnu.org/copyleft/gpl.html
-
+;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+;; for more details. http://www.gnu.org/copyleft/gpl.html
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs. If you did not, write to the Free Software
 ;; Foundation, Inc., 675 Mass Ave., Cambridge, MA 02139, USA.
@@ -36,6 +36,9 @@
 ;;; History:
 ;;
 ;; $Log: google.el,v $
+;; Revision 1.4  2002/10/06 21:06:06  lawrence
+;; Integrated summer changes, see ChangeLog for details.
+;;
 ;; Revision 1.3  2002/06/17 17:57:39  lawrence
 ;; Added copyright notice.
 ;;
@@ -49,50 +52,46 @@
 
 ;;; Code:
 
-;; provide a working `replace-in-string'
-(eval-and-compile
-  (cond
-   ((fboundp 'replace-in-string)
-    (defalias 'google-replace-in-string 'replace-in-string))
-   ((fboundp 'replace-regexp-in-string)
-    (defun google-replace-in-string  (string regexp newtext &optional literal)
-      (replace-regexp-in-string regexp newtext string nil literal)))
-   (t
-    (defun google-replace-in-string (string regexp newtext &optional literal)
-      (let ((start 0) tail)
-	(while (string-match regexp string start)
-	  (setq tail (- (length string) (match-end 0)))
-	  (setq string (replace-match newtext nil literal string))
-	  (setq start (- (length string) tail))))
-      string))))
-
 
 (defun google (string &optional type option)
   "Google for STRING.
 
 TYPE is the type of search (a symbol), one of:
-   www --- search www.google.com.
+   search --- search www.google.com.
    groups --- search groups.google.com.
+   direct --- search the Google directory.
+   images --- search images.google.com.
 
 OPTION is an option for groups.google.com searches only (a symbol), one of:
-   mid --- search for a message-id.
+   selm --- search for a message-id.
    group --- go to a group."
   (interactive "sGoogle: ")
-  (browse-url (concat "http://"
-		      (if type (symbol-name type)"www")".google.com/"
-		      (cond ((eq type 'www)
-			      "search?hl=en&q")
-			    ((eq type 'groups)
-			     (concat "groups?hl=en"
-				     (cond ((eq option 'mid)
-					    "&selm")
-					   ((eq option 'group)
-					    "&group")
-					   (t "&q"))))
-			    (t "search?hl=en&q"))
-		      "="
-		      (google-replace-in-string string " " "+") "")))
+  (let ((dir (eq type 'direct))
+        (type (symbol-name (if (or (null type) (eq type 'direct))
+                               'search
+                             type)))
+        (option (and option (symbol-name option))))
+    (browse-url (concat "http://www.google.com/"
+                        type
+                        "?"
+                        (or option "q")
+                        "="
+                        (google-make-sendable-string string)
+                        (and dir "&cat=gwd/Top")))))
 
+
+(defun google-make-sendable-string (string)
+  "Make STRING sendable as part of a Google URL.
+
+This converts each character in STRING to its hex representation
+preceded by a \"%\".
+
+e.g.
+\(google-make-sendable-string \"foo\")
+    => \"%66%6f%6f\"."
+  (mapconcat #'(lambda (c)
+                 (format "%%%02X" c))
+             (string-to-list string) ""))
 
 (defun google-groups (string)
   "Search for STRING on groups.google.com."
@@ -105,37 +104,40 @@ OPTION is an option for groups.google.com searches only (a symbol), one of:
 
 Defaults to the newsgroup at point."
   (interactive)
-  (let ((group
-	 (condition-case nil
-	     (google-replace-in-string (thing-at-point 'url) "^http://" "")
-	   (error nil))))
-    (google (read-string (format "Which newsgroup (default is %s): " group)
-			 nil nil group) 'groups 'group)))
+  (let* ((group (thing-at-point 'url))
+         (group (and group
+                    (string-match "^http://" group)
+                    (replace-match "" nil t group))))
+    (google
+     (read-string (if group
+                      (format "Which newsgroup (default %s): " group)
+                    "Which newsgroup: ")
+                  nil nil group)
+     'groups 'group)))
 
 
 (defun google-groups-message-id (start end)
   "Google Groups for the message-id between START and END."
   (interactive "r")
-  (google (buffer-substring-no-properties start end) 'groups 'mid))
+  (google (buffer-substring-no-properties start end) 'groups 'selm))
 
 
 (defun google-region (start end)
   "Google for text from START to END."
   (interactive "r")
-  (google (buffer-substring-no-properties start end) 'www))
+  (google (buffer-substring-no-properties start end) 'search))
 
 
 (defun google-sentence ()
   "Google for sentence at point."
   (interactive)
-  (google (thing-at-point 'sentence) 'www))
+  (google (thing-at-point 'sentence) 'search))
 
 
 (defun google-word ()
   "Google for word at point."
   (interactive)
-  (google (thing-at-point 'word) 'www))
-
+  (google (thing-at-point 'word) 'search))
 
 
 (provide 'google)
